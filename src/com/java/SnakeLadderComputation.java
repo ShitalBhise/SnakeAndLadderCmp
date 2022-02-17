a8cfef43426dddb5757ec74c046f77ad2b2d8fe8
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-17 19:32:04 +0530

    Added: Option checked logic for NoPlay-Ladder-Snake

--- src/com/java/SnakeLadderComputation.java
+++ src/com/java/SnakeLadderComputation.java
@@ -5,6 +5,9 @@ import java.util.Random;
 public class SnakeLadderComputation {
 
 	static int position = 0;
+	static final int NO_PLAY = 0;
+	static final int LADDER = 1;
+	static final int SNAKE = 2;
 
 	public static int RollDice() {
 		Random Random = new Random();
@@ -12,8 +15,27 @@ public class SnakeLadderComputation {
 		return diceRoll;
 	}
 
+	public static void optionCheck(int diceRoll) {
+		Random random = new Random();
+		int option = random.nextInt(3);
+
+		// Ladder
+		if (option == LADDER) {
+			position += diceRoll;
+		}
+		// Snak
+		else if (option == SNAKE) {
+			position -= diceRoll;
+		} else {
+			position = position;
+		}
+	}
+
 	public static void main(String[] args) {
+		int diceRoll = RollDice();
 		System.out.println("diceRoll= " + RollDice());
+		optionCheck(diceRoll);
+		System.out.println("position = " + position);
 
 	}
 
