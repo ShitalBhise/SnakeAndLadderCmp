3b103c92f60844a588e9c9a9957fda93178b0925
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-17 18:29:24 +0530

    Added: Dice Roll Logic for getting 1-6

--- src/com/java/SnakeLadderComputation.java
+++ src/com/java/SnakeLadderComputation.java
@@ -1,10 +1,19 @@
 package com.java;
 
+import java.util.Random;
+
 public class SnakeLadderComputation {
 
 	static int position = 0;
 
+	public static int RollDice() {
+		Random Random = new Random();
+		int diceRoll = Random.nextInt(6) + 1;
+		return diceRoll;
+	}
+
 	public static void main(String[] args) {
+		System.out.println("diceRoll= " + RollDice());
 
 	}
 
